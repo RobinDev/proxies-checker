f3fcfadaf71757c8da5cb0c15cec5c153fb4ae10
RobinDev <contact@robin-d.fr>
2014-11-11 18:39:23 +0100

    Edit `Fork me on Github` link

--- index.php
+++ index.php
@@ -138,5 +138,5 @@ code {background-color: #f9f2f4;border-radius: 4px;color: #c7254e;font-size: 90%
             <p style="text-align:center;margin-top:300px;"><?=$lang['poweredBy']?> <a href="http://www.robin-d.fr/">Robin (Consultant SEO et Développeur PHP)</a>
         </p>
     </div>
-    <a href="https://github.com/you"><img style="position: absolute; top: 0; right: 0; border: 0;" src="https://camo.githubusercontent.com/52760788cde945287fbb584134c4cbc2bc36f904/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f77686974655f6666666666662e706e67" alt="Fork me on GitHub" data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_right_white_ffffff.png"></a>
+    <a href="https://github.com/RobinDev/proxies-checker"><img style="position: absolute; top: 0; right: 0; border: 0;" src="https://camo.githubusercontent.com/52760788cde945287fbb584134c4cbc2bc36f904/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f77686974655f6666666666662e706e67" alt="Fork my proxies checker (PHP script) on GitHub" data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_right_white_ffffff.png"></a>
 </body>
